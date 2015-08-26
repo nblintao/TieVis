c4cb8d931b73a078ef417b478a09eb501041d1c3
Tao LIN <nblintao@126.com>
2015-08-26 13:02:52 +0800

    prepare for the change to new data format

--- code/wave/js/edgewave.js
+++ code/wave/js/edgewave.js
@@ -53,136 +53,149 @@ var edgeWaveDate = [
   }
 ];
 
+var nodes = [{ "name": "node1" }, { "name": "node2" }, { "name": "node3" }, { "name": "node4" }];
+
 var options = {
-  matrixwave: false
+  matrixwave: false,
+  dataset: 'smalldata'
 };
 
-var svg = d3.select('body').append('svg')
-  .attr('width', innerWidth)
-  .attr('height', innerHeight);
-
-var skewsvg = svg.append('g')
-  .attr('transform', function () {
-    if (options.matrixwave) {
-      return 'translate(50 200) rotate(-45) ';
-    }
-    else {
-      return 'translate(50 200)';
-    }
+d3.json(options.dataset + '/edgeWaveData.json', function (edgeWaveData) {
+  d3.json(options.dataset + '/nodelist.json', function (nodelist) {
+    d3.json(options.dataset + '/details.json', function (details) {
+      console.log(edgeWaveData);
+      render();
+    });
   });
+});
 
-var gridSideLength = 200;
+function render() {
+  var svg = d3.select('body').append('svg')
+    .attr('width', innerWidth)
+    .attr('height', innerHeight);
 
-var grid = skewsvg.selectAll('g')
-  .data(edgeWaveDate)
-  .enter().append('g')
-  .attr('transform', function (d) {
-    var id = d.timeID;
-    if (options.matrixwave) {
-      var odd = id % 2;
-      var translateX = Math.floor(id / 2) * gridSideLength;
-      var translateY = Math.ceil(id / 2) * gridSideLength;
-      var str = 'translate(' + translateX + ' ' + translateY + ')';
-      if (odd) {
-        // str += 'translate(' + gridSideLength + ' ' + gridSideLength + ') scale(-1 0)';
-        str += ' rotate(-90) scale(-1 1)';
+  var skewsvg = svg.append('g')
+    .attr('transform', function () {
+      if (options.matrixwave) {
+        return 'translate(50 200) rotate(-45) ';
       }
-      return str;
-    }
-    else {
-      return 'translate(' + id * gridSideLength + ' 0)';
-    }
-  });
-
-// grid.append('rect')
-//   .attr('width', gridSideLength - 30)
-//   .attr('height', gridSideLength - 30)
-//   .attr('x', 0)
-//   .attr('y', 0);
-
-// grid.append('circle')
-//   .attr('r', 10)
-//   .attr('cx', 10)
-//   .attr('cy', 10)
-//   .attr('fill', 'blue');
-// grid.append('circle')
-//   .attr('r', 10)
-//   .attr('cx', 10)
-//   .attr('cy', 190)
-//   .attr('fill', 'red');
-// grid.append('circle')
-//   .attr('r', 10)
-//   .attr('cx', 190)
-//   .attr('cy', 190)
-//   .attr('fill', 'yellow');
-
-var nodes = [{ "name": "node1" }, { "name": "node2" }, { "name": "node3" }, { "name": "node4" }];
-var nNodes = nodes.length;
-
-var innerGridSideLength = gridSideLength - 30;
-var cellOffset = d3.scale.ordinal().rangeBands([0, innerGridSideLength]);
-cellOffset.domain(d3.range(nNodes));
-
-var row = grid.selectAll(".row")
-  .data(function (d) {
-
-    var matrix = [];
-    nodes.forEach(function (node, i) {
-      matrix[i] = d3.range(nNodes).map(function (j) { return { x: j, y: i, z: 0 }; });
+      else {
+        return 'translate(50 200)';
+      }
     });
-    d.edges.forEach(function (edge) {
-      matrix[edge[0]][edge[1]].z = edge[2];
+
+  var gridSideLength = 200;
+
+  var grid = skewsvg.selectAll('g')
+    .data(edgeWaveDate)
+    .enter().append('g')
+    .attr('transform', function (d) {
+      var id = d.timeID;
+      if (options.matrixwave) {
+        var odd = id % 2;
+        var translateX = Math.floor(id / 2) * gridSideLength;
+        var translateY = Math.ceil(id / 2) * gridSideLength;
+        var str = 'translate(' + translateX + ' ' + translateY + ')';
+        if (odd) {
+          // str += 'translate(' + gridSideLength + ' ' + gridSideLength + ') scale(-1 0)';
+          str += ' rotate(-90) scale(-1 1)';
+        }
+        return str;
+      }
+      else {
+        return 'translate(' + id * gridSideLength + ' 0)';
+      }
     });
-    // console.log(matrix);
-    return matrix;
 
-  })
-  .enter().append('g')
-  .attr('class', 'row')
-  .attr('id', function (d, i) { return 'row' + i; })
-  .attr('transform', function (d, i) { return 'translate(0,' + cellOffset(i) + ')'; });
+  // grid.append('rect')
+  //   .attr('width', gridSideLength - 30)
+  //   .attr('height', gridSideLength - 30)
+  //   .attr('x', 0)
+  //   .attr('y', 0);
+
+  // grid.append('circle')
+  //   .attr('r', 10)
+  //   .attr('cx', 10)
+  //   .attr('cy', 10)
+  //   .attr('fill', 'blue');
+  // grid.append('circle')
+  //   .attr('r', 10)
+  //   .attr('cx', 10)
+  //   .attr('cy', 190)
+  //   .attr('fill', 'red');
+  // grid.append('circle')
+  //   .attr('r', 10)
+  //   .attr('cx', 190)
+  //   .attr('cy', 190)
+  //   .attr('fill', 'yellow');
+
+  var nNodes = nodes.length;
+
+  var innerGridSideLength = gridSideLength - 30;
+  var cellOffset = d3.scale.ordinal().rangeBands([0, innerGridSideLength]);
+  cellOffset.domain(d3.range(nNodes));
+
+  var row = grid.selectAll(".row")
+    .data(function (d) {
+
+      var matrix = [];
+      nodes.forEach(function (node, i) {
+        matrix[i] = d3.range(nNodes).map(function (j) { return { x: j, y: i, z: 0 }; });
+      });
+      d.edges.forEach(function (edge) {
+        matrix[edge[0]][edge[1]].z = edge[2];
+      });
+      // console.log(matrix);
+      return matrix;
+
+    })
+    .enter().append('g')
+    .attr('class', 'row')
+    .attr('id', function (d, i) { return 'row' + i; })
+    .attr('transform', function (d, i) { return 'translate(0,' + cellOffset(i) + ')'; });
 
 
-// row.append("line")
-//   .attr("x2", width);
+  // row.append("line")
+  //   .attr("x2", width);
 
-row.append("text")
-  .attr("x", -2)
-  .attr("y", cellOffset.rangeBand() / 2)
-  .attr("dy", ".32em")
-  .attr("text-anchor", "end")
-  .text(function (d, i) { return nodes[i].name; });
+  row.append("text")
+    .attr("x", -2)
+    .attr("y", cellOffset.rangeBand() / 2)
+    .attr("dy", ".32em")
+    .attr("text-anchor", "end")
+    .text(function (d, i) { return nodes[i].name; });
 
-var cell = row.selectAll(".cell")
-// .data(row.filter(function (d) { return d.z; }))
-  .data(function (d) {
-    // console.log(d);
-    return d;
-  })
-  .enter().append("rect")
-  .attr("class", "cell")
-  .attr('id', function (d) { return 'col' + d.x; })
-  .attr("x", function (d) { return cellOffset(d.x); })
-  .attr("width", cellOffset.rangeBand())
-  .attr("height", cellOffset.rangeBand())
-  .style("fill", function (d) { return d3.rgb(0, d.z / 7 * 255, 0); })
-  .on('mouseover', function (d) {
-    // console.log(d);
-    //   var fromPoint = d.y;
-    //   var toPoint = d.x;
-    svg.selectAll('#row' + d.y)
-      .selectAll('#col' + d.x)
-      .style('fill-opacity', 0.5)
-    // .style('stroke-width',10)
-      .style('stroke', 'red')
+  var cell = row.selectAll(".cell")
+  // .data(row.filter(function (d) { return d.z; }))
+    .data(function (d) {
+      // console.log(d);
+      return d;
+    })
+    .enter().append("rect")
+    .attr("class", "cell")
+    .attr('id', function (d) { return 'col' + d.x; })
+    .attr("x", function (d) { return cellOffset(d.x); })
+    .attr("width", cellOffset.rangeBand())
+    .attr("height", cellOffset.rangeBand())
+    .style("fill", function (d) { return d3.rgb(0, d.z / 7 * 255, 0); })
+    .on('mouseover', function (d) {
+      // console.log(d);
+      //   var fromPoint = d.y;
+      //   var toPoint = d.x;
+      svg.selectAll('#row' + d.y)
+        .selectAll('#col' + d.x)
+        .style('fill-opacity', 0.5)
+      // .style('stroke-width',10)
+        .style('stroke', 'red')
+      ;
+      // .each(function (c) { c.style('fill-opacity', 0.5); });
+    })
+    .on('mouseout', function (d) {
+      svg.selectAll('#row' + d.y)
+        .selectAll('#col' + d.x)
+        .style('fill-opacity', 1)
+        .style('stroke', '');
+
+    })
     ;
-    // .each(function (c) { c.style('fill-opacity', 0.5); });
-  })
-  .on('mouseout', function (d) {
-    svg.selectAll('#row' + d.y)
-      .selectAll('#col' + d.x)
-      .style('fill-opacity', 1)
-      .style('stroke', '');
-
-  })
-  ;
+};
