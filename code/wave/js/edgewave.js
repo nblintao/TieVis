2912ad9896290def0afea5a1cd13369167d1c39f
Tao LIN <nblintao@126.com>
2015-08-26 16:25:07 +0800

    add kdeView

--- code/wave/js/edgewave.js
+++ code/wave/js/edgewave.js
@@ -56,7 +56,7 @@
 // var nodes = [{ "name": "node1" }, { "name": "node2" }, { "name": "node3" }, { "name": "node4" }];
 
 var options = {
-  matrixwave: false,
+  matrixwave: true,
   // dataset: 'smalldata'
   dataset: 'largedata'
 };
@@ -71,7 +71,7 @@ d3.json(options.dataset + '/edgeWaveData.json', function (edgeWaveData) {
 });
 
 function render(edgeWaveData, nodelist, details) {
-  var svg = d3.select('body').append('svg')
+  var svg = d3.select('#timeView').append('svg')
     .attr('width', innerWidth * 6)
     .attr('height', innerHeight * 2);
 
@@ -81,7 +81,7 @@ function render(edgeWaveData, nodelist, details) {
         return 'translate(50 200) rotate(-45) ';
       }
       else {
-        return 'translate(50 200)';
+        return 'translate(50 0)';
       }
     });
 
@@ -190,9 +190,9 @@ function render(edgeWaveData, nodelist, details) {
     .attr("x", function (d) { return cellOffset(d.x); })
     .attr("width", cellOffset.rangeBand())
     .attr("height", cellOffset.rangeBand())
-    .style("fill", function (d) { return d3.rgb(0, d.v / 8 * 255, 0); })
+    // .style("fill", function (d) { return d3.rgb(0, d.v / 100 * 255, 0); })
+    .style("fill", function (d) { return d3.hsl(120,d.v/50,0.5); })
     .on('mouseover', function (d) {
-      // console.log(d);
       //   var fromPoint = d.y;
       //   var toPoint = d.x;
       svg.selectAll('#row' + d.y)
@@ -202,6 +202,10 @@ function render(edgeWaveData, nodelist, details) {
         .style('stroke', 'red')
       ;
       // .each(function (c) { c.style('fill-opacity', 0.5); });
+      
+      var kdeData = details[d.i];
+      // console.log(kdeData);
+      renderKDE(kdeData);
     })
     .on('mouseout', function (d) {
       svg.selectAll('#row' + d.y)
@@ -212,17 +216,20 @@ function render(edgeWaveData, nodelist, details) {
     })
     ;
 };
-renderKDE();
+
+// renderKDE();
+
 // reference: http://bl.ocks.org/mbostock/4341954
-function renderKDE() {
+function renderKDE(faithful) {
   var margin = { top: 20, right: 30, bottom: 30, left: 40 },
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 500 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
   var x = d3.scale.linear()
-    .domain([30, 110])
+  // .domain([30, 110])
+    .domain([d3.min(faithful), d3.max(faithful)])
     .range([0, width]);
-
+  // console.log([d3.min(faithful), d3.max(faithful)])
   var y = d3.scale.linear()
     .domain([0, .1])
     .range([height, 0]);
@@ -244,7 +251,10 @@ function renderKDE() {
     .frequency(false)
     .bins(x.ticks(40));
 
-  var svg = d3.select("body").append("svg")
+
+  var svg = d3.select("#kdeView");
+  svg.selectAll('svg').remove();
+  svg = svg.append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
@@ -259,13 +269,13 @@ function renderKDE() {
     .attr("x", width)
     .attr("y", -6)
     .style("text-anchor", "end")
-    .text("Time between Eruptions (min.)");
+    .text("Impact");
 
   svg.append("g")
     .attr("class", "y axis")
     .call(yAxis);
 
-  var faithful = [79, 54, 74, 62, 85, 55, 88, 85, 51, 85, 54, 84, 78, 47, 83, 52, 62, 84, 52, 79, 51, 47, 78, 69, 74, 83, 55, 76, 78, 79, 73, 77, 66, 80, 74, 52, 48, 80, 59, 90, 80, 58, 84, 58, 73, 83, 64, 53, 82, 59, 75, 90, 54, 80, 54, 83, 71, 64, 77, 81, 59, 84, 48, 82, 60, 92, 78, 78, 65, 73, 82, 56, 79, 71, 62, 76, 60, 78, 76, 83, 75, 82, 70, 65, 73, 88, 76, 80, 48, 86, 60, 90, 50, 78, 63, 72, 84, 75, 51, 82, 62, 88, 49, 83, 81, 47, 84, 52, 86, 81, 75, 59, 89, 79, 59, 81, 50, 85, 59, 87, 53, 69, 77, 56, 88, 81, 45, 82, 55, 90, 45, 83, 56, 89, 46, 82, 51, 86, 53, 79, 81, 60, 82, 77, 76, 59, 80, 49, 96, 53, 77, 77, 65, 81, 71, 70, 81, 93, 53, 89, 45, 86, 58, 78, 66, 76, 63, 88, 52, 93, 49, 57, 77, 68, 81, 81, 73, 50, 85, 74, 55, 77, 83, 83, 51, 78, 84, 46, 83, 55, 81, 57, 76, 84, 77, 81, 87, 77, 51, 78, 60, 82, 91, 53, 78, 46, 77, 84, 49, 83, 71, 80, 49, 75, 64, 76, 53, 94, 55, 76, 50, 82, 54, 75, 78, 79, 78, 78, 70, 79, 70, 54, 86, 50, 90, 54, 54, 77, 79, 64, 75, 47, 86, 63, 85, 82, 57, 82, 67, 74, 54, 83, 73, 73, 88, 80, 71, 83, 56, 79, 78, 84, 58, 83, 43, 60, 75, 81, 46, 90, 46, 74];
+  // var faithful = [79, 54, 74, 62, 85, 55, 88, 85, 51, 85, 54, 84, 78, 47, 83, 52, 62, 84, 52, 79, 51, 47, 78, 69, 74, 83, 55, 76, 78, 79, 73, 77, 66, 80, 74, 52, 48, 80, 59, 90, 80, 58, 84, 58, 73, 83, 64, 53, 82, 59, 75, 90, 54, 80, 54, 83, 71, 64, 77, 81, 59, 84, 48, 82, 60, 92, 78, 78, 65, 73, 82, 56, 79, 71, 62, 76, 60, 78, 76, 83, 75, 82, 70, 65, 73, 88, 76, 80, 48, 86, 60, 90, 50, 78, 63, 72, 84, 75, 51, 82, 62, 88, 49, 83, 81, 47, 84, 52, 86, 81, 75, 59, 89, 79, 59, 81, 50, 85, 59, 87, 53, 69, 77, 56, 88, 81, 45, 82, 55, 90, 45, 83, 56, 89, 46, 82, 51, 86, 53, 79, 81, 60, 82, 77, 76, 59, 80, 49, 96, 53, 77, 77, 65, 81, 71, 70, 81, 93, 53, 89, 45, 86, 58, 78, 66, 76, 63, 88, 52, 93, 49, 57, 77, 68, 81, 81, 73, 50, 85, 74, 55, 77, 83, 83, 51, 78, 84, 46, 83, 55, 81, 57, 76, 84, 77, 81, 87, 77, 51, 78, 60, 82, 91, 53, 78, 46, 77, 84, 49, 83, 71, 80, 49, 75, 64, 76, 53, 94, 55, 76, 50, 82, 54, 75, 78, 79, 78, 78, 70, 79, 70, 54, 86, 50, 90, 54, 54, 77, 79, 64, 75, 47, 86, 63, 85, 82, 57, 82, 67, 74, 54, 83, 73, 73, 88, 80, 71, 83, 56, 79, 78, 84, 58, 83, 43, 60, 75, 81, 46, 90, 46, 74];
 
   var data = histogram(faithful),
     kde = kernelDensityEstimator(epanechnikovKernel(7), x.ticks(100));
