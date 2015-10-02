219ea4c02f7355e1f16cf1a2e3b1c2a18655cd8f
Tao LIN <nblintao@126.com>
2015-10-02 15:34:30 +0800

    update toolbox icon

--- code/EdgeViewer/views/control.js
+++ code/EdgeViewer/views/control.js
@@ -1,11 +1,13 @@
 var ControlPanel = Backbone.View.extend({
 	initialize:function() {
 		this.html = '<div class="btn-group-vertical btn-group-sm" role="group">'+
-				'<button type="button" class="btn btn-default" aria-label="Left Align" id = "brush" data-toggle="button">'+
-	 				'<span class="glyphicon glyphicon-screenshot" aria-hidden="true"></span>' +
+				'<button type="button" class="btn btn-default" aria-label="Left Align" id = "brush" data-toggle="button" style="background-image:url(Iconset/Select.png);width:40px;height:40px;background-size:40px;">'+
+	 				// '<span class="glyphicon glyphicon-screenshot" aria-hidden="true"></span>' +
+					// '<img src="Iconset/Select.png" height="20px" width="20px"/>' +
 				'</button>' +
-				'<button type="button" class="btn btn-default" aria-label="Left Align" id = "stick" data-toggle="button">' +
-	 				'<span class="glyphicon glyphicon-pushpin" aria-hidden="true"></span>' +
+				'<button type="button" class="btn btn-default" aria-label="Left Align" id = "stick" data-toggle="button" style="background-image:url(Iconset/Lock.png);width:40px;height:40px;background-size:40px;">' +
+	 				// '<span class="glyphicon glyphicon-pushpin" aria-hidden="true"></span>' +
+					//  '<img src="Iconset/Lock.png" height="20px" width="20px"/>' +
 				'</button>' +				
 			'</div>';
 	},
